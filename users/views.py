7ba5aee0da6517e25828efc60a737d7e82b56274
Vagner Elena <dipnoe@gmail.com>
2023-08-26 01:57:26 +0300

    update for user

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, UpdateAPIView
+
+from users.models import User
+from users.serializers import UserSerializer
+
 
 # Create your views here.
+class UserListAPIView(ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
