8a8561f25307c060056c222a4d87e5705c19d052
Vagner Elena <dipnoe@gmail.com>
2023-08-27 02:47:50 +0300

    add FK to course

--- lesson/models.py
+++ lesson/models.py
@@ -13,11 +13,15 @@ class Lesson(models.Model):
     - description (TextField): Description of the lesson.
     - preview (ImageField): Preview image of the lesson.
     - video_link (URLField): Link to the lesson video.
+    - course (ForeignKey): ForeignKey to model Course.
     """
     name = models.CharField(max_length=100, verbose_name='название')
     description = models.TextField(verbose_name='описание', **NULLABLE)
     preview = models.ImageField(verbose_name='первью', **NULLABLE)
     video_link = models.URLField(max_length=200, verbose_name='ссылка на видео', **NULLABLE)
+
+    course = models.ForeignKey('course.Course', on_delete=models.CASCADE, related_name='lessons',
+                               verbose_name='курс', **NULLABLE)
 
     def __str__(self):
         return self.name
