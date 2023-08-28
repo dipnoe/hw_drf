ec8dfa564afc206e01047a63a2f897b458c8b02f
Vagner Elena <dipnoe@gmail.com>
2023-08-28 19:59:46 +0300

    add admin register

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from course.models import Course
+
+
 # Register your models here.
+@admin.register(Course)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'owner',)
