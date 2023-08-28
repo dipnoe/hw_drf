a82fc449d4a4f1af2c16fcf9a96d1e4be4d63ad6
Vagner Elena <dipnoe@gmail.com>
2023-08-28 20:01:05 +0300

    add paths to get a token

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
 from users.views import *
@@ -9,4 +10,8 @@ urlpatterns = [
     path('', UserListAPIView.as_view(), name='user_list'),
     path('create/', UserCreateAPIView.as_view(), name='user_create'),
     path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='user_update'),
+
+    # paths to get a token
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh')
 ]
