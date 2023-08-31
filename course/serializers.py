d56582e022a191b2c48c06388621e5b51bb69f06
Vagner Elena <dipnoe@gmail.com>
2023-08-31 23:48:36 +0300

    new serializer SubscriptionSerializer

--- course/serializers.py
+++ course/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from course.models import Course
+from course.models import Course, Subscription
 from lesson.serializers import LessonSerializer
 
 
@@ -16,10 +16,29 @@ class CourseSerializer(serializers.ModelSerializer):
     """
     lesson_count = serializers.SerializerMethodField()
     lessons = LessonSerializer(many=True, required=False)
+    is_subscribe = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
-        fields = ('name', 'description', 'preview', 'lesson_count', 'lessons')
+        fields = ('name', 'description', 'preview', 'is_subscribe', 'lesson_count', 'lessons')
 
     def get_lesson_count(self, instance):
         return instance.lessons.count()
+
+    def get_is_subscribe(self, instance):
+        request = self.context['request']
+        subscription = Subscription.objects.filter(course=instance, user=request.user)
+        return True if subscription else False
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Subscription model.
+
+    Fields:
+    - course (ForeignKey): The course associated with the subscription.
+    - user (ForeignKey): The user who subscribed to the course.
+    """
+    class Meta:
+        model = Subscription
+        fields = '__all__'
