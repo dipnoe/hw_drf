a484c9de24d5b7455593b65d9b4545b2c5354c70
Vagner Elena <dipnoe@gmail.com>
2023-08-31 23:50:04 +0300

    add urls for subscription

--- course/urls.py
+++ course/urls.py
@@ -2,15 +2,14 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from course.apps import CourseConfig
-from course.views import CourseViewSet
+from course.views import *
 
 app_name = CourseConfig.name
-
 
 router = DefaultRouter()
 router.register(r'course', CourseViewSet, basename='course')
 
-
 urlpatterns = [
-
+      path('course/subscription/create/', SubscriptionCreateAPIView.as_view(), name='create_subscription'),
+      path('course/subscription/delete/<int:pk>/', SubscriptionDeleteAPIView.as_view(), name='delete_subscription'),
 ] + router.urls
